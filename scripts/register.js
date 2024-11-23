30215c2272d0b9a3b9425e5b6ca9a0a0158adf53
alexandrov5433 <alexandrov5433@gmail.com>
2024-11-23 21:51:46 +0100

    Loader animation and password generation added.

--- scripts/register.js
+++ scripts/register.js
@@ -1,25 +1,39 @@
 const regForm = document.querySelector('#registrationForm');
 const createAccBtn = document.querySelector('#createAccBtn');
 const errorMsgEl = document.querySelector('#errorMsg');
+const loader = document.querySelector('div.loader-container');
+const randPassBtn = document.querySelector('#randomPass');
+const passwordInput = document.querySelector('#passwordInput');
+const repeatPasswordInput = document.querySelector('#repeatPasswordInput');
+
 
 createAccBtn.addEventListener('click', async () => {
+    toggleLoaderAndBtns();
     const formData = Object.fromEntries(new FormData(regForm));
     console.log(formData);
     const errors = generalValidation(formData);
     inputErrorClassToggle(errors);
     if (Object.keys(errors).length) {
+        toggleLoaderAndBtns();
         errorMsgEl.textContent = genErrMsg(errors);
         return;
     } 
-    const res = await preloads.register(formData);
+    // const res = await preloads.register(formData);
     console.log('register.js ', res);
     
     //error acc exists;
     if (res instanceof Error) {
         errorMsgEl.textContent = res.message;
-    } 
+    }
+    toggleLoaderAndBtns(); 
 });
 
+randPassBtn.addEventListener('click', async () => {
+    const generatedPass = await preloads.generatePassword();
+    passwordInput.value = generatedPass;
+    repeatPasswordInput.value = generatedPass;
+    lightUpPasswordInput();
+});
 
 function generalValidation(formData) {
     const errors = {};
@@ -63,4 +77,19 @@ function inputErrorClassToggle(errors) {
             f.classList.remove('error');
         }
     });   
+}
+
+function toggleLoaderAndBtns() {
+    loader.classList.toggle('hide');
+    createAccBtn.classList.toggle('hide');
+    randPassBtn.classList.toggle('hide');
+}
+
+function lightUpPasswordInput() {
+    passwordInput.classList.add('password-incerted');
+    repeatPasswordInput.classList.add('password-incerted');
+    setTimeout(() => {
+        passwordInput.classList.remove('password-incerted');
+        repeatPasswordInput.classList.remove('password-incerted');
+    }, 401);
 }
